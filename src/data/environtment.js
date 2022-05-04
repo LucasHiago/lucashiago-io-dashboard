5a7373f6d68e22358cd9101d33a21ab64f7bcbef
Lucas Hiago <lucas.hiago@jet.com.br>
2022-05-04 16:56:53 -0300

    right path

--- src/data/environtment.js
+++ src/data/environtment.js
@@ -1,4 +1,4 @@
 import { readable } from "svelte/store";
 
 //export const urlEnv = readable('http://localhost:5000');
-export const urlEnv = readable('https://api-lucashiago-github-io.herokuapp.com/');
+export const urlEnv = readable('https://api-lucashiago-github-io.herokuapp.com');
