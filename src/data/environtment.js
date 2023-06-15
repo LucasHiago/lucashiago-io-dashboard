cdf79d3717cdd3487a13c917b7072c5c98ada02c
LucasHDSF <lucashiago.dsf@gmail.com>
2023-06-15 16:35:03 -0300

    Update environtment.js

--- src/data/environtment.js
+++ src/data/environtment.js
@@ -1,4 +1,4 @@
 import { readable } from "svelte/store";
 
 //export const urlEnv = readable('http://localhost:5000');
-export const urlEnv = readable('https://squadops.herokuapp.com');
+export const urlEnv = readable('https://sqdapi.squadevops.com');
